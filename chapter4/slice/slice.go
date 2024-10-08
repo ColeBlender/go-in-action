6c08541eadd01e7a325e7138ce44886bd8405dc1
Cole Blender <coleblender@gmail.com>
2024-10-08 12:57:34 -0700

    dank

--- chapter4/slice/slice.go
+++ chapter4/slice/slice.go
@@ -16,5 +16,24 @@ func Slice() {
 	fmt.Println(s4)
 
 	s5 := []int{1, 2, 3, 4, 5}
-	fmt.Println(s5[1:3])
+	s6 := s5[1:3]
+	// fmt.Println(s5[1:3])
+
+	s6 = append(s6, 69)
+	fmt.Println(s5, s6)
+
+	s7 := []int{1, 2, 3, 4, 5, 6, 7}
+	s8 := s7[1:2:3]
+	fmt.Println(s8)
+	s9 := append(s7, s8...)
+	fmt.Println(s9)
+
+	for i, x := range s9 {
+		fmt.Println(i, x)
+	}
+
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+	}
+
 }
